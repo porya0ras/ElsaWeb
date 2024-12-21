43b10b12da0828747ffa8216e35ab9b792827da6
Porya_ras <Porya_ras@DESKTOP-CORSC6I>
2024-12-21 21:30:58 +0000

    Development: add elsa.UseWorkflowsApi()

--- Program.cs
+++ Program.cs
@@ -5,22 +5,31 @@ using ElsaWeb.Services;
 using ElsaWeb.Services.Abstracts;
 using ElsaWeb.WorkFlows;
 using Microsoft.EntityFrameworkCore;
+using Parlot.Fluent;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-builder.Services.AddDbContext<MainDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+services.AddDbContext<MainDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 builder.Services.AddElsa(elsa =>
 {
     // workflows by assembly
     elsa.AddWorkflowsFrom<Program>();
+    //elsa.UseWorkflowsApi();
+
     elsa.UseHttp();
+    elsa.UseSasTokens();
 });
 
 builder.Services.AddScoped<IDataAccessService, DataAccessService>();
 
 var app = builder.Build();
 
+// Expose Elsa APIs.
+//app.UseWorkflowsApi();
+
+// Add Elsa HTTP middleware (to handle requests mapped to HTTP Endpoint activities)
 app.UseWorkflows();
 
 app.MapGet("/", () => "Hello World!");
